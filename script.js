f5c441f9975f3f9c9b303057db6f660cbb246c89
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-01-04 11:35:48 +0800

    feat: RenderBoard 32 by 32 but still user can't choose their own RenderBoard
    Create render Board

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let board = document.querySelector('.board');
+const boardSize = document.querySelector('.board-size');
+const btnSize = document.querySelector('.btn-size');
 
+
+
+
+function renderBoard(){
+    
+    board.style.gridTemplateColumns = `repeat(32, 1fr)`;
+    board.style.gridTemplateRows = `repeat(32, 1fr)`;
+
+    for(let i = 0; i < (32 * 32); i++){
+        let boardItem = document.createElement('div');
+        boardItem.classList.add(`boardItem`);
+        boardItem.style.height = `32px`;
+        boardItem.style.backgroundColor = 'lightGray';
+        board.appendChild(boardItem)
+    }
+}
+
+renderBoard()
