dec6b37aafe6216f7fc5460700ba7404933f99f0
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-01-04 12:19:08 +0800

    feat: Add functionality on choose size input for user to choose board size
    Create functionality on choose size for user to select their own preference size board.

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 let board = document.querySelector('.board');
-const boardSize = document.querySelector('.board-size');
+const boardSize = document.querySelector('#board-size');
 const btnSize = document.querySelector('.btn-size');
+const sizeError = document.querySelector('.size-error');
 
 
 
 
-function renderBoard(){
+function renderBoard(size){
     
-    board.style.gridTemplateColumns = `repeat(32, 1fr)`;
-    board.style.gridTemplateRows = `repeat(32, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for(let i = 0; i < (32 * 32); i++){
+    for(let i = 0; i < (size * size); i++){
         let boardItem = document.createElement('div');
         boardItem.classList.add(`boardItem`);
-        boardItem.style.height = `32px`;
+        boardItem.style.height = `20px`;
         boardItem.style.backgroundColor = 'lightGray';
         board.appendChild(boardItem)
     }
 }
 
-renderBoard()
+renderBoard(32)
+
+btnSize.addEventListener('click', ()=>{
+    const size = boardSize.value;
+    if(size >= 16 && size <= 100){  
+        renderBoard(size);
+
+    } else{
+        sizeError.textContent = `The size should be at least 16 and not higher than 100`;
+
+        size.sizeError.style.color = 'crimson'
+    }
+});
+
+
+
